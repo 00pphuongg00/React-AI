047d434fc3be44ce38f8ed43631926704b9b3ff1
phuongcoder <phuongbape29@gmail.com>
2022-01-14 16:01:53 +0700

    React-AI

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './brand.css';
+import { slack, shopify, atlassian, dropbox, google } from './imports'
+
+const Brand = () => {
+    return (
+        <div className="gpt3__brand section__padding">
+            <div>
+                <img src={google} alt="Google" />
+            </div>
+            <div>
+                <img src={slack} alt="Slack" />
+            </div>
+            <div>
+                <img src={atlassian} alt="Atlassian" />
+            </div>
+            <div>
+                <img src={dropbox} alt="Dropbox" />
+            </div>
+            <div>
+                <img src={shopify} alt="Shopify" />
+            </div>
+
+        </div>
+    )
+}
+
+export default Brand
+
+
+
